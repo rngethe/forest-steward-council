dca9e39d6a20176e95738797998a25356603f601
rngethe <52126985+rngethe@users.noreply.github.com>
2020-08-23 22:30:49 +0300

    fixed update sheets

--- src/components/Sheet.js
+++ src/components/Sheet.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Card, Container, theme } from '@hackclub/design-system'
 
 const Sheet = styled(Card.withComponent(Container))`
   position: relative;
