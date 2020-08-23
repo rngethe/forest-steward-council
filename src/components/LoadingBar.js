2fb63aca527ad93821f876ada08a6689b9033b9b
rngethe <52126985+rngethe@users.noreply.github.com>
2020-08-23 21:45:40 +0300

    updated

--- src/components/LoadingBar.js
+++ src/components/LoadingBar.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import React from 'react'
+import { Box, Loading } from '@hackclub/design-system'
 import PropTypes from 'prop-types'
 
 const Base = styled(Box)`
