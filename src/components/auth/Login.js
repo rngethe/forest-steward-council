1afc02fd0a01e5215134b089dea249498605faca
rngethe <52126985+rngethe@users.noreply.github.com>
2020-08-23 21:56:16 +0300

    updated

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Flex, Heading } from '@hackclub/design-system'
 import styled from 'styled-components'
 import Helmet from 'react-helmet'
 import Flag from 'components/Flag'
