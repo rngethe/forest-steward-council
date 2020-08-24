2fc088d9525007943e5b4d7ef71db3debdf28c6a
rngethe <52126985+rngethe@users.noreply.github.com>
2020-08-24 20:21:33 +0300

    fixed loading bar

--- src/components/LoadingBar.js
+++ src/components/LoadingBar.js
@@ -3,22 +3,51 @@ import React from 'react'
 import { Box, Loading } from '@hackclub/design-system'
 import PropTypes from 'prop-types'
 
-const Base = styled(Box)`
-  position: relative;
-  ${props => props.fill && { height: '100vh' }};
-  div {
-    border-radius: 50%;
+const Spinner = () => (
+  <StyledSpinner viewBox="0 0 50 50">
+    <circle
+      className="path"
+      cx="25"
+      cy="25"
+      r="20"
+      fill="none"
+      strokeWidth="4"
+    />
+  </StyledSpinner>
+);
+
+const LoadingBar = styled.svg`
+  animation: rotate 2s linear infinite;
+  margin: -25px 0 0 -25px;
+  width: 50px;
+  height: 50px;
+  
+  & .path {
+    stroke: #5652BF;
+    stroke-linecap: round;
+    animation: dash 1.5s ease-in-out infinite;
   }
-`
+  
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes dash {
+    0% {
+      stroke-dasharray: 1, 150;
+      stroke-dashoffset: 0;
+    }
+    50% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -35;
+    }
+    100% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -124;
+    }
+  }
+`;
 
-const LoadingBar = props => (
-  <Base py={5} {...props}>
-    <Loading />
-  </Base>
-)
-
-LoadingBar.propTypes = {
-  fill: PropTypes.bool
-}
 
 export default LoadingBar
