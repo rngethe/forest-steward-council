8f2ad46299800ffd6a8545762cd6988092217360
rngethe <52126985+rngethe@users.noreply.github.com>
2020-08-26 07:55:18 +0300

    updated url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import fetch from 'isomorphic-fetch'
 import storage from 'storage'
 
-export const url = 'https://docusign.netlify.app/'
+export const url = 'https://docusign.netlify.app/.netlify/identity/'
 
 const methods = ['GET', 'PUT', 'POST', 'PATCH', 'DELETE']
 
