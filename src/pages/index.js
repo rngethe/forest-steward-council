f7d5ed3c26d2e0f4e64ed793c4f0b21988637d5e
rngethe <52126985+rngethe@users.noreply.github.com>
2020-08-27 00:33:21 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,18 +71,6 @@ export default class extends Component {
     }
   //}
 
-const netlifyAuth = {
-  isAuthenticated: false,
-  user: null,
-  authenticate(callback) {
-    this.isAuthenticated = true;
-    netlifyIdentity.open();
-    netlifyIdentity.on('login', user => {
-      this.user = user;
-      callback(user);
-    });
-  },
-
   render() {
     return (
       <Layout>
