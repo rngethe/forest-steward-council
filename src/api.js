f8260e0e199337411e2335922731c2e3c4cb63b2
rngethe <52126985+rngethe@users.noreply.github.com>
2020-08-27 00:38:15 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -7,4 +7,36 @@ window.netlifyIdentity = netlifyIdentity;
 // You must run this once before trying to interact with the widget
 netlifyIdentity.init();
 
+const netlifyAuth = {
+  isAuthenticated: false,
+  user: null,
+  authenticate(callback) {
+    this.isAuthenticated = true;
+    netlifyIdentity.open();
+    netlifyIdentity.on('login', user => {
+      this.user = user;
+      callback(user);
+    });
+  },
+  
+  function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        netlifyAuth.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
 export default api
