caee69af35cca3c578e82e53f02ac0674fd15324
rngethe <52126985+rngethe@users.noreply.github.com>
2020-08-28 01:59:37 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import Layout from 'components/Layout'
 import ApplyNav from 'components/apply/ApplyNav'
 import Sheet from 'components/Sheet'
 import Main from 'components/apply/Main'
-//import api from 'api'
+import api from 'api'
 import { useAuth0 } from '@auth0/auth0-react';
 //import EmailLoginForm from 'components/auth/EmailLoginForm'
 import LoadingBar from 'components/LoadingBar'
